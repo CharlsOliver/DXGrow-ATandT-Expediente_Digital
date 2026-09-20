9045aeaaffb75608f959e9b7c2da4137817b1d15
CharlsOliver <charlsoliver96@gmail.com>
2026-09-20 01:00:27 +0000

    Ajuste obtener usuario en sesion SSFF

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,22 +1,11 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "btpexpedientedigital/model/models",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator",
-    "sap/m/MessageBox"
-], (
-    UIComponent,
-    models,
-    JSONModel,
-    Filter,
-    FilterOperator,
-    MessageBox
-) => {
+    "sap/ui/model/json/JSONModel"
+], (UIComponent, models, JSONModel) => {
     "use strict";
 
     return UIComponent.extend("btpexpedientedigital.Component", {
-
         metadata: {
             manifest: "json",
             interfaces: [
@@ -25,149 +14,57 @@ sap.ui.define([
         },
 
         init() {
-            // Call the base component's init function
             UIComponent.prototype.init.apply(this, arguments);
 
-            // Set the device model
             this.setModel(models.createDeviceModel(), "device");
 
-            // Enable routing
             this.getRouter().initialize();
 
-            // Obtener usuario en sesión
+            // Cargar usuario autenticado
             this.userReady = this._loadBaseUser();
         },
 
-        /**
-         * Obtiene el usuario autenticado desde XSUAA/User API.
-         * En ejecución local utiliza un usuario de prueba.
-         */
         _loadBaseUser: function () {
-            return new Promise((resolve) => {
-
-                fetch("/user-api/currentUser")
-                    .then((oResponse) => {
-                        if (!oResponse.ok) {
-                            return Promise.reject();
-                        }
-
-                        return oResponse.json();
-                    })
-                    .then((oUser) => {
-
-                        const oBaseUser = {
-                            email: oUser.email || oUser.name,
-                            name: oUser.firstname,
-                            lastname: oUser.lastname,
-                            source: "XSUAA"
-                        };
-
-                        this.setModel(
-                            new JSONModel(oBaseUser),
-                            "User"
-                        );
-
-                        return this._loadSfUserData(oBaseUser.email);
-                    })
-                    .then(() => {
-                        resolve();
-                    })
-                    .catch(() => {
-
-                        // Usuario para pruebas locales
-                        const oLocalUser = {
-                            email: "local@bas.dev",
-                            name: "Test User",
-                            lastname: "",
-                            source: "LOCAL",
-
-                            // Cambiar por el usuario que quieras probar
-                            user_id: "1071137"
-                        };
-
-                        this.setModel(
-                            new JSONModel(oLocalUser),
-                            "User"
-                        );
-
-                        resolve();
-                    });
-            });
-        },
-
-        /**
-         * Busca en SuccessFactors al usuario obtenido desde XSUAA
-         * utilizando su correo electrónico.
-         */
-        _loadSfUserData: function (sEmail) {
-            return new Promise((resolve) => {
-
-                const oSFSF = this.getModel("SFSF");
-                const oUserModel = this.getModel("User");
-
-                const aFilters = [
-                    new Filter(
-                        "email",
-                        FilterOperator.EQ,
-                        sEmail
-                    )
-                ];
-
-                oSFSF.read("/User", {
-                    filters: aFilters,
-
-                    urlParameters: {
-                        "$select": "userId,email,firstName,lastName",
-                        "$format": "json"
-                    },
-
-                    success: (oData) => {
-
-                        if (oData.results.length > 0) {
-
-                            const oSfUser = oData.results[0];
-
-                            oUserModel.setProperty(
-                                "/user_id",
-                                oSfUser.userId
-                            );
-
-                            oUserModel.setProperty(
-                                "/firstName",
-                                oSfUser.firstName
-                            );
-
-                            oUserModel.setProperty(
-                                "/lastName",
-                                oSfUser.lastName
-                            );
-
-                            console.log(
-                                "Usuario en sesión:",
-                                oUserModel.getData()
-                            );
-
-                        } else {
-
-                            MessageBox.warning(
-                                `No se encontró el usuario ${sEmail} en SuccessFactors.`
-                            );
-                        }
-
-                        resolve();
-                    },
-
-                    error: (oError) => {
-                        console.error(
-                            "Error obteniendo usuario de SuccessFactors:",
-                            oError
-                        );
-
-                        resolve();
+            return fetch("/user-api/currentUser")
+                .then((oResponse) => {
+                    if (!oResponse.ok) {
+                        throw new Error("User API no disponible");
                     }
-                });
-            });
-        }
 
+                    return oResponse.json();
+                })
+                .then((oUser) => {
+                    const oUserData = {
+                        email: oUser.email || oUser.name || "",
+                        name: oUser.firstname || "",
+                        lastname: oUser.lastname || "",
+                        source: "XSUAA"
+                    };
+
+                    this.setModel(
+                        new JSONModel(oUserData),
+                        "User"
+                    );
+
+                    return oUserData;
+                })
+                .catch(() => {
+
+                    // Usuario para ejecución local en BAS
+                    const oUserData = {
+                        email: "local@bas.dev",
+                        name: "Test User",
+                        lastname: "",
+                        source: "LOCAL"
+                    };
+
+                    this.setModel(
+                        new JSONModel(oUserData),
+                        "User"
+                    );
+
+                    return oUserData;
+                });
+        }
     });
 });
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,22 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller"
 ], (BaseController) => {
-  "use strict";
+    "use strict";
 
-  return BaseController.extend("btpexpedientedigital.controller.App", {
-      onInit() {
-      }
-  });
+    return BaseController.extend("btpexpedientedigital.controller.App", {
+
+        onInit() {
+            const bInsideShell =
+                document.documentElement.classList.contains(
+                    "sapUiInsideFioriShell"
+                );
+
+            const oStandaloneHeader = this.byId("standaloneHeader");
+
+            if (oStandaloneHeader) {
+                oStandaloneHeader.setVisible(!bInsideShell);
+            }
+        }
+
+    });
 });
